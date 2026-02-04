84379c6f45049bd91a2e98e9dca66a9a068c4c0b
PRbrate <pauloteles08@hotmail.com>
2026-02-04 09:39:23 -0300

    Adicionar arquivos de projeto.

---
+++ ShotUrl/Model/EntityUrl.cs
@@ -0,0 +1,9 @@
+﻿namespace ShotUrl.Model
+{
+    public class EntityUrl
+    {
+        public string ShortId { get; set; }
+        public string BaseUrl { get; set; }
+        public DateTime Created_at { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ ShotUrl/Repository/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShotUrl.Model;
+
+namespace ShotUrl.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<EntityUrl> EntityUrls { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<EntityUrl>().HasIndex(u => u.ShortId).IsUnique(true);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
